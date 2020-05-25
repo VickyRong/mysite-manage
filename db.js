15b6da02a1ec615b46e44d67501187fe7a30664e
yanrong.hu <yanrong.hu@weimob.com>
2020-05-25 16:09:13 +0800

    增加编辑接口

--- db.js
+++ db.js
@@ -1,88 +1,79 @@
+const { ObjectId } = require('mongodb');
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/Mysite";
+var DB = "Mysite";
 
-MongoClient.connect(url,{ useNewUrlParser:true },function(err,db){
-    if(err) throw err;
-    var dbo = db.db("mysite");
-        //【建立collection】
-        // dbo.createCollection('blog', function (err, res) {
-        //     if (err) throw err;
-        //     console.log("创建集合!");
-        //     db.close();
-        // });
-        //【插入一条数据】
-        // var myObj = { name:'包小胡同学',sex:'女' };
-        // dbo.collection("blog").insertOne(myObj,function(err,res){
-        //     if(err) throw err;
-        //     console.log("文档插入成功!");
-        //     db.close();
-        // })
-        //【插入多条数据】
-        // var myobj =  [
-        //     { name: 'Wechat', url: 'https://www.wechat.com', type: 'cn'},
-        //     { name: 'Google', url: 'https://www.google.com', type: 'en'},
-        //     { name: 'Facebook', url: 'https://www.google.com', type: 'en'},
-        //     { name: 'Taobao', url: 'https://www.taobao.com', type: 'en'},
-        //     { name: 'Baidu', url: 'https://www.baidu.com', type: 'en'},
-        //    ];
-        // dbo.collection("blog").insertMany(myobj, function(err, res) {
-        //     if (err) throw err;
-        //     console.log("插入的文档数量为: " + res.insertedCount);
-        //     db.close();
-        // });
-        //【查询数据】
-        // dbo.collection("blog").find({}).toArray(function(err,result){
-        //     if(err) throw err;
-        //     console.log("result:",result);
-        //     db.close();
-        // })
-        //【查询指定条件的数据】
-        // var whereStr = {'name':'菜鸟工具'};
-        // dbo.collection('blog').find(whereStr).toArray(function(err,result){
-        //     if(err) throw err;
-        //     console.log("res:",result);
-        //     db.close()
-        // })
-        //【更新数据】
-        // var whereStr = {'name':'菜鸟工具'};
-        // var updateStr = {$set:{ "url": "https://www.baidu.com" }};
-        // dbo.collection('blog').updateOne(whereStr,updateStr,function(){
-        //     if(err) throw err;
-        //     console.log('文档更新成功！');
-        //     db.close();
-        // })
-        //【更新多条数据】
-        // var whereStr = { type:'en' } //查询条件
-        // var updateStr = { $set:{ "url":"https:www.taobao.com" }};
-        // dbo.collection('blog').updateMany(whereStr,updateStr,function(err,res){
-        //     if(err) throw err;
-        //     console.log(res.result.nModified + ' 条文档被更新');
-        //     db.close();
-        // })
-        //【删除一条数据】
-        // var whereStr = {"name":"菜鸟工具"};
-        // dbo.collection('blog').deleteOne(whereStr,function(err,obj){
-        //     if(err) throw err;
-        //     console.log('文档删除成功');
-        //     db.close();
-        // })
-        //【删除多条数据】
-        // var whereStr = { type:'en' };
-        // dbo.collection('blog').deleteMany(whereStr,function(err,obj){
-        //     if(err) throw err;
-        //     console.log(obj.result.n + ' 条文档被删除');
-        //     db.close();
-        // })
-        //查询分页      
-        // dbo.collection('blog').find().limit(2).toArray(function(err,result){
-        //     if(err) throw err;
-        //     console.log(result);
-        //     db.close();
-        // })  
-        // dbo.collection('blog').find().skip(2).limit(2).toArray(function(err,result){
-        //     if(err) throw err;
-        //     console.log(result);
-        //     db.close();
-        // })  
+var serverOperate = {
+    //新增一条数据
+    insertOneRecord: (document,data,cb) => {
+        collectionToDb(document,function(target){
+            target.insertOne(data,function(err,res){
+                if(err) throw err;
+                cb && cb('成功插入单条记录！')
+            })
+        })
+    },
+    
+    //新增多条数据
+    insertManyRecord: (document,data,cb) => {
+        collectionToDb(document,function(target){
+            target.insertMany(data,function(err,res){
+                if(err) throw err;
+                cb && cb('成功插入多条记录！')
+            })
+        })
+    },
+     
+    //查询数据
+    queryRecord: (document,data,cb) =>{
+        collectionToDb(document,function(target){
+            var whereStr = data.whereStr ? data.whereStr : {};
+            target.find(whereStr).toArray(function(err,result){
+                if(err) throw err;
+                cb && cb(result)
+            })
+        })
+    },
 
-})
+    //删除数据
+    deleteRecord: (document,data,cb) =>{
+        collectionToDb(document,function(target){
+            var whereStr = { _id: ObjectId(data.id) }
+            target.deleteOne(whereStr,function(err,obj){
+                if(err) throw err;
+                cb && cb('数据删除成功!')
+            })
+        })
+    },
+
+    //编辑数据
+    updateRecord: (document,data,cb) =>{
+        collectionToDb(document,function(target){
+            var whereStr = { _id: ObjectId(data.id) }
+            var updateObj = data;
+            delete updateObj._id;
+            var updateStr = {$set:updateObj};
+            target.updateOne(whereStr,updateStr,function(err,obj){
+                if(err) throw err;
+                cb && cb('数据更新成功!')
+            })
+        })
+    }
+}
+
+//连接数据库
+ collectionToDb = (document,cb) =>{
+    MongoClient.connect(url,{ useNewUrlParser:true },function(err,db){
+        if(err) throw err;
+        var dbo = db.db(DB);
+        var target = dbo.collection(document);
+        cb && cb(target)
+        db.close();
+    })
+}
+
+module.exports = serverOperate;
+
+
+
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var db = require('./db');
 
 //设置跨域访问--貌似没用，浏览器使用cors插件
 app.all('*', function(req, res, next) {
@@ -17,17 +18,55 @@ app.use(bodyParser.urlencoded({ extended:false }))
 //应用application/json格式
 app.use(bodyParser.json());
 
+//新增用户
+app.post('/addUser',function(req,res){
+    var params = req.body || {};
+    db.insertOneRecord('user',params,function(data){
+        res.send({res:data})
+    })
+})
 
-// app.get('/saveMdFile',urlencodedParser,function(req,res){
-//     // console.log("req~~~~~~:",req);
-//     console.log("query:",req.query);
-//     console.log("body-------:",req.body);
-// })
+//删除用户
+app.post('/deleteUser',function(req,res){
+    var params = req.body || {};
+    db.deleteRecord('user',params,function(data){
+        res.send({res:data})
+    })
+})
 
-app.post('/saveMdFile',function(req,res){
-    console.log("req~~~~~~:",req);
-    console.log("query:",req.query);
-    console.log("body-------:",req.body);
+//查询用户
+app.post('/queryUser',function(req,res){
+    var params = req.body || {};
+    db.queryRecord('user',params,function(data){
+        res.send({res:data})
+    })
+})
+
+//更新用户
+app.post('/updateUser',function(req,res){
+    var params = req.body || {};
+    console.log('更新参数--------：',params)
+    db.updateRecord('user',params,function(data){
+        res.send({res:data})
+    })
+})
+
+
+
+//查询文章
+app.post('/queryBlog',function(req,res){
+    var params = req.body || {};
+    db.queryRecord('blog',params,function(data){
+        res.send({res:data})
+    })
+})
+
+//删除文章
+app.post('/deleteBlog',function(req,res){
+    var params = req.body || {};
+    db.deleteRecord('blog',params,function(data){
+        res.send({res:data})
+    })
 })
 
 var server = app.listen(8082,function(){
